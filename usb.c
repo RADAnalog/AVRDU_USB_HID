8f63d4cf48bed26520c626d4946b3ccfd65aa6e6
Richard <richardadzioba@gmail.com>
2026-05-23 09:48:38 -0700

    Add files via upload

--- usb.c
+++ usb.c
@@ -3,24 +3,28 @@
 //
 //	Minimal HID implementation with Interrupt EP1.IN and EP1.OUT
 //
-// Multipacket mode is used for the control EP0 IN endpoint. After a USB RESET,
-// the first Get_Descriptor request is special: the host expects only the first
-// 8 bytes of the 18?byte Device Descriptor. If EP0 IN has a BUFSIZE smaller
-// than the full descriptor (e.g., 8 or 16 bytes), the buffer will contain extra
-// bytes the host did not request. This prevents TRNCOMPL from ever asserting,
-// causing the endpoint busy_wait loop to stick. The workaround is to use a BUFSIZE
-// larger than the Device Descriptor length (i.e., =32 or = 64).
+//  Multipacket mode is used for the control EP0 IN endpoint to allow descriptors
+//	larger than BUFSIZE.  After a USB RESET, the host expects only 8 bytes total on
+//	the first Get_Descriptor request, and ignores the remaining 10 byte of the
+//	descriptor.  If EP0.IN BUFSIZE is less than the full descriptor size, the 
+//	multipacket processing will not complete properly and TRNCOMPL will not assert.
+//  This will cause an infinite endpoint busy_wait loop. The workaround is to use a BUFSIZE
+//  larger than the Device Descriptor length (i.e., =32 or = 64).
+//
+//	2026-05-12 - To mitigate the above issue somewhat, exit wait loop if INTFLAGSA.RESET.
 //
 //	If using the Control Endpoint for data transfer (HidGet/Set Report), the
 //	the EP0.IN is used for *both* IN and OUT transactions.
 //
-//	Note: Control packet: 21 0a 0000 0000 0000 = "SET_IDLE" is not implemented (Stalled)
+//	80 06 0600 0000 000A = "Get Device Qualifier" not implemented for single speed devices.
+//	21 0a 0000 0000 0000 = "HID Set Idle" - means NAK IN unless data is ready to send to host.
 //
 //	Pin7 - Configured LED - On when device is configured and ready
 //	Pin6 - Suspended LED - On when Host signals suspend mode
 //
-//	Author: Richard
-//	Date:	2026-04-13
+//	Author:		Richard
+//	Date:		2026-04-13
+//	Updated:	2026-05-23 - minor comments clarifications
 //
 //*****************************************************************************
 #include "config.h"				// F_CPU
@@ -32,84 +36,87 @@
 #include <string.h>				// memcpy
 #include <stddef.h>				// wchar_t
 #include "usb.h"				// USB Common functions
-#include "usb_private.h"		// USB helper private functions
+#include "usb_private.h"		// USB Private functions
 #include "led.h"				// LED functions
 #include "descriptor.h"			// Descriptor
 
-//	Define Endpoint table
+//	Define Endpoint table (struct is word aligned)
 Usb_Endpoint_Table_t endpointTable;
 
 //	Define Endpoint buffers
 Usb_SetupPacket_t setupPacket;	// EP0.OUT 8 byte Setup packet
-uint8_t controlPacket[64];		// EP0.IN/OUT Control packet
+uint8_t controlPacket[64];		// EP0.IN and EP0.OUT Control packet
 uint8_t ep1InPacket[8];			// EP1.IN packet
 uint8_t ep1OutPacket[8];		// EP1.OUT packet
 
-// USB peripheral configured and ready flag
+// USB peripheral 'Configured' status
 volatile bool usbStateConfigured = false;
 
 
 //*****************************************************************************
 //	Initialize USB interface
+//	- Configure USB peripheral
+//	- Configure Endpoints
+//	- Enable Device and Endpoint interrupts
 //*****************************************************************************
 void usbInit(void)
 {
-	// Enable USB module
+	// Configure USB peripheral
 	
 	// Enable internal 3.3 V regulator
 	SYSCFG.VUSBCTRL = SYSCFG_USBVREG_bm;
 	
-	// Set the maximum endpoint number used, and enable USB peripheral
+	// Set the maximum endpoint number and enable USB peripheral
 	USB0.CTRLA = USB_ENABLE_bm | EP_MAX_ADDR;
 
 	// Wait for PLL to lock
 	usbWaitPllLock();		
 	
 
-	// Initialize Endpoint Table pointer
+	// Initialize Endpoints
+	
+	// Initialize Endpoint table pointer
 	USB0.EPPTR = (uint16_t)endpointTable.EP;
 	
-	
-	// Initialize EP0.OUT (Setup) Host -> Device
-	endpointTable.EP[EP0].OUT.STATUS	=	0;
+	// Initialize EP0.OUT (Setup)
 	endpointTable.EP[EP0].OUT.CTRL		=	USB_TYPE_CONTROL_gc |	
 											USB_TCDSBL_bm |					// Disable global interrupt sync
 											USB_BUFSIZE_DEFAULT_BUF8_gc;	// Setup packet is always 8 bytes
 	endpointTable.EP[EP0].OUT.DATAPTR	=	(uint16_t)&setupPacket;			// Setup packet buffer
+	endpointTable.EP[EP0].OUT.STATUS	=	0;
 	endpointTable.EP[EP0].OUT.CNT		=	0;
 	endpointTable.EP[EP0].OUT.MCNT		=	0;
 	
-	// Initialize Control EP0.IN Device -> Host/Host -> Device
-	endpointTable.EP[EP0].IN.STATUS		=	0;
+	// Initialize EP0.IN (Control)
 	endpointTable.EP[EP0].IN.CTRL		=	USB_TYPE_CONTROL_gc |
 											USB_TCDSBL_bm |					// Disable global interrupt sync
 											USB_MULTIPKT_bm | USB_AZLP_bm |	// Multipacket and Automatic ZLP
 											USB_BUFSIZE_DEFAULT_BUF64_gc;	// Buffer size must match DeviceDescriptor.MaxPacketSize0
-	endpointTable.EP[EP0].IN.DATAPTR	=	(uint16_t)controlPacket;		// Control IN/OUT buffer (Datasheet 27.3.2.2 SETUP)
+	endpointTable.EP[EP0].IN.DATAPTR	=	(uint16_t)controlPacket;		// Control IN and OUT buffer (Datasheet 27.3.2.2 SETUP)
+	endpointTable.EP[EP0].IN.STATUS		=	0;
 	endpointTable.EP[EP0].IN.CNT		=	0;
 	endpointTable.EP[EP0].IN.MCNT		=	0;
 	
-
-	// Initialize Interrupt EP1.IN Device -> Host
-	endpointTable.EP[EP1].IN.STATUS		=	0;
+	// Initialize Interrupt EP1.IN (Device -> Host)
 	endpointTable.EP[EP1].IN.CTRL		=	USB_TYPE_BULKINT_gc |
 											USB_BUFSIZE_DEFAULT_BUF8_gc;
 	endpointTable.EP[EP1].IN.DATAPTR	=	(uint16_t)ep1InPacket;
+	endpointTable.EP[EP1].IN.STATUS		=	0;
 	endpointTable.EP[EP1].IN.CNT		=	0;
 	endpointTable.EP[EP1].IN.MCNT		=	0;	
 	
-	// Initialize Interrupt EP1 OUT Host -> Device
-	endpointTable.EP[EP1].OUT.STATUS	=	0;
+	// Initialize Interrupt EP1 OUT (Host -> Device)
 	endpointTable.EP[EP1].OUT.CTRL		=	USB_TYPE_BULKINT_gc |
 											USB_BUFSIZE_DEFAULT_BUF8_gc;
 	endpointTable.EP[EP1].OUT.DATAPTR	=	(uint16_t)ep1OutPacket;
+	endpointTable.EP[EP1].OUT.STATUS	=	0;
 	endpointTable.EP[EP1].OUT.CNT		=	0;
 	endpointTable.EP[EP1].OUT.MCNT		=	0;	
 	
 	
-	// Enable USB Bus and Endpoint interrupts
+	// Enable USB Device and Endpoint interrupts
 	
-	// Enable Bus interrupts
+	// Enable Device interrupts
 	USB0.INTCTRLA = USB_RESET_bm | USB_STALLED_bm;
 
 	// Enable Endpoint interrupts
@@ -188,14 +195,15 @@ ISR(USB0_BUSEVENT_vect)
 //*****************************************************************************
 ISR(USB0_TRNCOMPL_vect)
 {
-	// Setup
+	// Setup packet received
 	if (USB0.INTFLAGSB & USB_SETUP_bm)
 	{
 		USB0.INTFLAGSB = USB_SETUP_bm;
 		
+		// Clear endpoint flags
 		usbClearSetup();
 		
-		// Handle request
+		// Handle setup request
 		usbHandleSetupRequest();
 		
 		return;
@@ -206,23 +214,20 @@ ISR(USB0_TRNCOMPL_vect)
 	{
 		USB0.INTFLAGSB = USB_TRNCOMPL_bm;
 		
-		// Send packet to host on HidReport.bInterval frequency
+		// Send packet to host according to HidReport.bInterval
 		if (endpointTable.EP[EP1].IN.STATUS & USB_TRNCOMPL_bm) 
 		{	
 			usbClearInTransaction(EP1);
 		
 			// Load packet with data to be sent to host on next EP1.IN request
-			//ep1InPacket[0]++;
-			//ep1InPacket[1]++;
+			//ep1InPacket[0] = your data here;
+			//ep1InPacket[1] = your data here;
 			
 			// Set buffer length
 			endpointTable.EP[EP1].IN.CNT = 2;
 			
-			// Print what is being sent
-			//printf("EP1.IN=%02x, %02x\n", ep1InPacket[0], ep1InPacket[1]);
-			
-			// Ack IN transfer
-			usbAckIn(EP1);
+			// Ack IN transfer to host - only when data is available
+			//usbAckIn(EP1);
 			
 			return;
 		}
@@ -232,10 +237,14 @@ ISR(USB0_TRNCOMPL_vect)
 		{	
 			usbClearOutTransaction(EP1);
 			
-			// Data received from EP1.OUT transaction is available
-			printf("EP1.OUT=%02x, %02x\n", ep1OutPacket[0], ep1OutPacket[1]);		
+			// Print data received from EP1.OUT transaction
+			printf("EP1.OUT=%02x, %02x\n", ep1OutPacket[0], ep1OutPacket[1]);	
 			
-			// Ack OUT transfer
+			// Test: loop back to IN from this OUT packet
+			ep1InPacket[0] = ep1OutPacket[0];
+			ep1InPacket[1] = ep1OutPacket[1];			
+			
+			// Ack OUT transfer from host
 			usbAckOut(EP1);
 			
 			return;
@@ -244,7 +253,7 @@ ISR(USB0_TRNCOMPL_vect)
 }
 
 //*****************************************************************************
-//	Decode and process the Setup Request
+//	Process the Setup Request
 //*****************************************************************************
 void usbHandleSetupRequest(void)
 {
@@ -252,7 +261,7 @@ void usbHandleSetupRequest(void)
 	printf("%02x %02x %04x %04x %04x\n", setupPacket.bmRequestType, setupPacket.bRequest, setupPacket.wValue, setupPacket.wIndex, setupPacket.wLength);
 	
 	// Decode Setup Request Type
-	switch (setupPacket.bmRequestType & BM_REQUEST_TYPE_MASK) // Mask is 01100000
+	switch (setupPacket.bmRequestType & BM_REQUEST_TYPE_MASK)	// 01100000b
 	{
 		case Device_Standard:
 			switch (setupPacket.bRequest)
@@ -267,7 +276,7 @@ void usbHandleSetupRequest(void)
 					usbSetConfiguration();
 					break;
 				default:
-					// Unsupported Standard_Request (bRequest)
+					// Stall unsupported Standard_Request (bRequest)
 					usbEnableStallRequest();
 					break;
 			}
@@ -281,14 +290,17 @@ void usbHandleSetupRequest(void)
 				case Hid_SetReport:
 					usbHidSetReport();
 					break;
+				case Hid_SetIdle:
+					usbHidSetIdle();
+					break;					
 				default:
-					// Unsupported Class_Request (bRequest)
+					// Stall unsupported Class_Request (bRequest)
 					usbEnableStallRequest();
 					break;
 			}
 			break;					
 		default:
-			// Unsupported Request_Type (bmRequestType)
+			// Stall unsupported Request_Type (bmRequestType)
 			usbEnableStallRequest();
 			break;
 	}
@@ -318,7 +330,7 @@ void usbGetDescriptor(void)
 			usbGetStringDescriptor();
 			break;			
 		default:
-			// Unsupported Descriptor_Type (wValueH)
+			// Stall unsupported Descriptor_Type (wValueH)
 			usbEnableStallRequest();
 			break;
 	}
@@ -345,9 +357,9 @@ void usbGetStringDescriptor(void)
 			usbSendDescriptor(&SerialNumberString, SerialNumberString.bLength);
 			break;
 		default:
-			// Unsupported String Index (wValueL)
+			// Stall unsupported String Index (wValueL)
 			usbEnableStallRequest();
-		break;
+			break;
 	}
 }
 
@@ -356,15 +368,15 @@ void usbGetStringDescriptor(void)
 //*****************************************************************************
 void usbSendDescriptor(const void *descriptor, uint16_t descriptorLength)
 {
-	// Data Stage
+	// -- Data Stage --
 	
-	// Use the minimum of Host requested length and descriptor length
+	// Use the minimum of requested length and descriptor length
 	uint16_t length = (setupPacket.wLength < descriptorLength) ? setupPacket.wLength : descriptorLength;
 	
 	// Copy descriptor to the EP0 buffer
 	memcpy(controlPacket, descriptor, length);
 	
-	// Set packet length
+	// Set data packet length
 	endpointTable.EP[EP0].IN.CNT = length;
 	endpointTable.EP[EP0].IN.MCNT = 0;
 	
@@ -372,6 +384,8 @@ void usbSendDescriptor(const void *descriptor, uint16_t descriptorLength)
 	usbAckIn(EP0);
 	usbWaitInTransactionComplete(EP0);
 	usbClearInTransaction(EP0);
+	
+	// -- Status Stage --
 	
 	// Ack Status Stage
 	usbAckOut(EP0);
@@ -385,17 +399,19 @@ void usbSendDescriptor(const void *descriptor, uint16_t descriptorLength)
 void usbSetAddress(void)
 {
 	// No Data Stage
+	
+	// -- Status Stage --
 
-	// Status Stage: reply with a ZLP
+	// Respond with ZLP
 	endpointTable.EP[EP0].IN.CNT = 0;
 	endpointTable.EP[EP0].IN.MCNT = 0;
 
-	// Ack Status Stage	
+	// Ack Status Stage
 	usbAckIn(EP0);
 	usbWaitInTransactionComplete(EP0);
 	usbClearInTransaction(EP0);
 
-	// Set the address
+	// Set this device address
 	usbSetDeviceAddress(setupPacket.wValueL);
 }
 
@@ -405,12 +421,14 @@ void usbSetAddress(void)
 void usbSetConfiguration(void)
 {
 	// No Data Stage
+	
+	// -- Status Stage -- 
 
-	// Status Stage: reply with a ZLP
+	// Respond with ZLP
 	endpointTable.EP[EP0].IN.CNT = 0;
 	endpointTable.EP[EP0].IN.MCNT = 0;
 	
-	// Ack Status Stage	
+	// Ack Status Stage
 	usbAckIn(EP0);
 	usbWaitInTransactionComplete(EP0);
 	usbClearInTransaction(EP0);
@@ -422,20 +440,40 @@ void usbSetConfiguration(void)
 }
 
 //*****************************************************************************
+//	Set Idle (bRequest = 0x0A) Host -> Device
+//*****************************************************************************
+void usbHidSetIdle(void)
+{
+	// No Data Stage
+	
+	// -- Status Stage --
+
+	// Respond with ZLP
+	endpointTable.EP[EP0].IN.CNT = 0;
+	endpointTable.EP[EP0].IN.MCNT = 0;
+		
+	// Ack Status Stage
+	usbAckIn(EP0);
+	usbWaitInTransactionComplete(EP0);
+	usbClearInTransaction(EP0);
+}
+
+//*****************************************************************************
 //	Hid Get Report (bRequest = 0x01) Device -> Host
 //*****************************************************************************
 void usbHidGetReport(void)
 {
-	// Data Stage 
+	// -- Data Stage  --
 	
-	// Provide example data:
-	controlPacket[0] = 0x01;
-	controlPacket[1] = 0x02;
+	// Load data to be sent to the host on this request, e.g.:
+	//controlPacket[0] = 0x01;
+	//controlPacket[1] = 0x02;
 	
-	// For EP0 Reports, the IN endpoint is used for both IN and OUT transactions.
-	// Print data to send to the Host over the Control Endpoint.
+	// For EP0 Reports the IN endpoint is used for *both* IN and OUT transactions.
+	// Debug - print data to send to the Host over EP0.
 	printf("GetReport Tx=%02x %02x\n", controlPacket[0], controlPacket[1]);
-
+	
+	// Set number of bytes to be sent to the host
 	endpointTable.EP[EP0].IN.CNT = 2;
 	endpointTable.EP[EP0].IN.MCNT = 0;
 
@@ -443,6 +481,8 @@ void usbHidGetReport(void)
 	usbAckIn(EP0);
 	usbWaitInTransactionComplete(EP0);
 	usbClearInTransaction(EP0);
+	
+	// -- Status Stage --
 
 	// Ack Status Stage
 	usbAckOut(EP0);
@@ -455,20 +495,24 @@ void usbHidGetReport(void)
 //*****************************************************************************
 void usbHidSetReport(void)
 {
+	// -- Data Stage --
+	
 	// Ack Data Stage
 	usbAckOut(EP0);
 	usbWaitOutTransactionComplete(EP0);
 	usbClearOutTransaction(EP0);
 		
-	// For EP0 Reports, the IN endpoint is used for both IN and OUT transactions.
+	// For EP0 Reports, the IN endpoint is used for *both* IN and OUT transactions.
 	// Print data received form the Host over the Control Endpoint.
 	printf("SetReport Rx=%02x %02x\n", controlPacket[0], controlPacket[1]);
+	
+	// -- Status Stage --
 
-	// Status Stage: reply with a ZLP
+	// Respond with ZLP
 	endpointTable.EP[EP0].IN.CNT = 0;
 	endpointTable.EP[EP0].IN.MCNT = 0;
 
-	// Ack Status IN Stage
+	// Ack Status Stage
 	usbAckIn(EP0);
 	usbWaitInTransactionComplete(EP0);
 	usbClearInTransaction(EP0);
